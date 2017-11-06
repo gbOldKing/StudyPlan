eff9ba676988740133c4fa37f825904b654e743a
oldking <261812135@qq.com>
2017-11-06 23:54:49 +0800

    '20171106'

--- angular/webapp-zpwz/gulpfile.js
+++ angular/webapp-zpwz/gulpfile.js
@@ -18,6 +18,7 @@ gulp.task('lib', function () {
     gulp.src('bower_components/**/*')
     .pipe(gulp.dest(app.devPath+'vendor'))
     .pipe(gulp.dest(app.prdPath+'vendor'))
+    .pipe($.connect.reload())
 });
 
 
@@ -25,5 +26,68 @@ gulp.task('html', function () {
     gulp.src(app.srcPath + '**/*.html')
     .pipe(gulp.dest(app.devPath))
     .pipe(gulp.dest(app.prdPath))
+    .pipe($.connect.reload())
 });
+
+gulp.task('json',function() {
+	gulp.src(app.srcPath + 'data/**/*.json')
+	.pipe(gulp.dest(app.devPath + 'data'))
+	.pipe(gulp.dest(app.prdPath + 'data'))
+    .pipe($.connect.reload())
+    .pipe($.connect.reload())
+});
+
+gulp.task('less', function () {
+    gulp.src(app.srcPath + 'style/index.less')
+    .pipe($.less())
+    .pipe(gulp.dest(app.devPath + 'css'))
+    .pipe($.cssmin())
+    .pipe(gulp.dest(app.prdPath + 'css'))
+    .pipe($.connect.reload())
+});
+
+gulp.task('js', function () {
+    gulp.src(app.srcPath + 'script/**/*.js')
+    .pipe($.concat('index.js'))
+    .pipe(gulp.dest(app.devPath + 'js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest(app.prdPath + 'js'))
+    .pipe($.connect.reload())
+});
+
+
+gulp.task('image', function () {
+    gulp.src(app.srcPath + 'images/**/*')
+    .pipe(gulp.dest(app.devPath +'images'))
+    .pipe($.imagemin())
+    .pipe(gulp.dest(app.prdPath+'images'))
+    .pipe($.connect.reload())
+});
+
+gulp.task('build',['lib','html','json','less','js','image']);
+
+gulp.task('clean', function () {
+    gulp.src([app.devPath,app.prdPath])
+    .pipe($.clean())
+});
+
+gulp.task('serve',['build'],function () {
+    $.connect.server({
+        root:[app.devPath],
+        livereload:true,
+        port:8888
+    });
+    open('http://localhost:8888');
+    gulp.watch('bower_components',['lib']);
+    gulp.watch(app.srcPath + '**/*.html',['html']);
+    gulp.watch(app.srcPath + 'data/**/*.json',['json']);
+    gulp.watch(app.srcPath + 'style/**/*.less',[';ess']);
+    gulp.watch(app.srcPath + 'script/**/*.js',['js']);
+    gulp.watch(app.srcPath + 'images/**/*',['image']);
+});
+
+gulp.task('default',['serve']);
+
+
+
 
---
+++ angular/webapp-zpwz/src/script/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/11/6.
+ */
+"use strict";
+
+angular.module('app',[]);
